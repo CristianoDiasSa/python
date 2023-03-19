56a9c83b2a2f13a116f0e6cc1ba5ce80dd9fc5be
Cristiano Dias Sa <Cristiano_ie@outlook.com>
2023-03-19 20:31:40 +0000

    added a function to read a txt file and search for a specific word and return how many times the word appears as well as the indexes of the ocurrences

--- functions.py
+++ functions.py
@@ -11,4 +11,24 @@ def print_diamond():
         print(" " * (rows - i), "*" * (2 * i + 1))
 
 
+def read_and_manipulate_text(word_to_search):
+    file = open('test.txt')
+    text = file.read()
+    file.close
+    split_text = text.split(' ')
+    print(split_text)
+    length = len(split_text)
+    count = split_text.count(word_to_search)
+    print('Total length of the text is ' + str(length) + ".",'\nThe total times that the word '+ word_to_search + 
+          " appears is " + str(count))
+    #This will return the indexes of all ocurrences of the searched word!
+    if count == 1:
+        print('The index of the searched word is ' + str(split_text.index(word_to_search)))
+    else:
+        ocurrences = []
+        for index, value in enumerate(split_text):
+            if value == word_to_search:
+                ocurrences.append(index)
+    
+        print('The indexes of the word searched is :' + str(ocurrences))
 
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import model as model
-import functions as functions
+import functions as fc
 
-
+"""
 first_person = model.Person('Cristiano', 29)
 second_person = model.Person('Dias', 29)
 people = [first_person, second_person]
 for person in people:
-    functions.print_details(person)
+    fc.print_details(person)
 
-functions.print_diamond()
+fc.print_diamond()
+"""
+fc.read_and_manipulate_text('the')
